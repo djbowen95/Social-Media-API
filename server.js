b354b516fb66a7b4242de47a172ec8a2bd7894b0
Daniel Bowen <djbowen95@gmail.com>
2022-05-10 15:05:57 +0100

    Create connection

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
