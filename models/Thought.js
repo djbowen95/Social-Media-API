947c0512a0780b34d6ac16809833cf4c93a1d687
Daniel Bowen <djbowen95@gmail.com>
2022-05-12 17:25:00 +0100

    Thought schema defines date, uses the Date object and the .now method

--- models/Thought.js
+++ models/Thought.js
@@ -6,9 +6,15 @@ const thoughtSchema = new Schema(
             type: String,
             required: true,
             max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
         }
     }
 )
+
+// Need a getter to format the date. 
 
 const Thought = model('thought', thoughtSchema);
 
