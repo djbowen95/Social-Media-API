c894bcfb0744cda6c1860281e157970bea2e2d19
Daniel Bowen <djbowen95@gmail.com>
2022-05-13 16:54:00 +0100

    Reaction schema defines username and createdAt properties

--- models/Reaction.js
+++ models/Reaction.js
@@ -10,6 +10,14 @@ const reactionSchema = new Schema(
             type: String,
             required: true,
             max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
         }
     }
 )
