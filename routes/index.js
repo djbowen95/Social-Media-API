828a18de7d9d367590e3f9e2c486e1262a92efe1
Daniel Bowen <djbowen95@gmail.com>
2022-05-14 15:30:42 +0100

    Import API routes to router

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const apiRoutes = require("/api");
+
+router.use("/api", apiRoutes);
+router.use((req, res) => res.send("Incorrect route provided."))
 
 module.exports = router;
