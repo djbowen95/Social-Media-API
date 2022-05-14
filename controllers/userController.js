adea3d487e4059628ae85a07f27cfc620d63c38c
Daniel Bowen <djbowen95@gmail.com>
2022-05-14 16:03:47 +0100

    API POST route to create a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
 const res = require("express/lib/response");
 const { User } = require("../models");
 
-const getAllUsers = () => {
-    User.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
-}
+const getAllUsers = (req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err));
+};
 
-module.exports = { getAllUsers }
+const createUser = (req, res) => {
+  User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+};
+
+module.exports = { getAllUsers, createUser };
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { getAllUsers } = require("../../controllers/userController");
+const { getAllUsers, createUser } = require("../../controllers/userController");
 
 router.route("/").get(getAllUsers);
+router.route("/").post(createUser);
 
 module.exports = router;
