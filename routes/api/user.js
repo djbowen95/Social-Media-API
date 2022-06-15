880f9be11b3f6e0661b67c385f69a8f52d6e904b
Daniel Bowen <djbowen95@gmail.com>
2022-06-15 16:36:45 +0100

    User routes for get all users, get one user and create new user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { getAllUsers, createUser } = require("../../controllers/userController");
+const { getAllUsers, createUser, getOneUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userController");
 
 router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userId").get(getOneUser)
 
 module.exports = router;
