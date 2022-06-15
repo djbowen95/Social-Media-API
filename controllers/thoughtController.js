238a4ec8c4332bb7b9f7809e17a91cd2c9d2df60
Daniel Bowen <djbowen95@gmail.com>
2022-06-15 16:46:45 +0100

    Controller function to create a new thought, and assign that thought to a user

---
+++ controllers/thoughtController.js
@@ -0,0 +1,24 @@
+const { json } = require("express");
+const { User, Thought } = require("../models");
+
+const thoughtController = {
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id } },
+                { new: true }
+            )
+        })
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: "Thought created but no user found with this ID."})
+            }
+            res.json("Thought successfully created.")
+        })
+        .catch((err) => res.status(500).json(err));
+    }
+}
+
+module.exports = thoughtController;
