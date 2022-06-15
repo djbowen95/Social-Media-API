4af79678b79cb1ea2d4dd5b3535b668840750fcf
Daniel Bowen <djbowen95@gmail.com>
2022-06-15 16:39:01 +0100

    User routes to update and delete a user using their id

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { getAllUsers, createUser, getOneUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userController");
 
 router.route("/").get(getAllUsers).post(createUser);
-router.route("/:userId").get(getOneUser)
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+
 
 module.exports = router;
