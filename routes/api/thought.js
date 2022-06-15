94987d1a61cd88d0b87ce6d0b09a4fdc026336bd
Daniel Bowen <djbowen95@gmail.com>
2022-06-15 16:47:19 +0100

    Thought route to create a new thought

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const { createThought } = require("../../controllers/thoughtController")
+
+router.route("/").post(createThought);
 
 module.exports = router;
