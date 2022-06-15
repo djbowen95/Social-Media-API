c1004dbc1c31b20f3103d2b9668d0f6d674bf0f7
Daniel Bowen <djbowen95@gmail.com>
2022-06-15 20:32:51 +0100

    Controller function for fetching all thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,23 +2,35 @@ const { json } = require("express");
 const { User, Thought } = require("../models");
 
 const thoughtController = {
-    createThought(req, res) {
-        Thought.create(req.body)
-        .then((thought) => {
-            return User.findOneAndUpdate(
-                { _id: req.body.userId },
-                { $addToSet: { thoughts: thought._id } },
-                { new: true }
-            )
-        })
-        .then((user) => {
-            if (!user) {
-                return res.status(404).json({ message: "Thought created but no user found with this ID."})
-            }
-            res.json("Thought successfully created.")
-        })
-        .catch((err) => res.status(500).json(err));
-    }
-}
+  getAllThoughts(req, res) {
+    Thought.find()
+      .select("-__v")
+      .then((allThoughts) => {
+        res.json(allThoughts);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) => {
+        if (!user) {
+          return res
+            .status(404)
+            .json({
+              message: "Thought created but no user found with this ID.",
+            });
+        }
+        res.json("Thought successfully created.");
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
 
 module.exports = thoughtController;
